057eacfb0ec5039011d72eccb0b6aa0206cccb70
Aleksandr.Eliseev <brgr18@yandex.ru>
2021-04-15 13:18:36 +0300

    change cap to new ChromeOptions()

--- src/test/java/selenium/docker/WithDockerTest.java
+++ src/test/java/selenium/docker/WithDockerTest.java
@@ -1,22 +1,18 @@
 package selenium.docker;
 
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import selenium.page.object.mailru.LetterPage;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 class WithDockerTest {
 
     @Test
     void createAndDeleteLetterTest() throws MalformedURLException {
-        RemoteWebDriver driver = new RemoteWebDriver(new URL("http://127.0.0.1:4444/wd/hub"), DesiredCapabilities.chrome());
+        RemoteWebDriver driver = new RemoteWebDriver(new URL("http://127.0.0.1:4444/wd/hub"), new ChromeOptions());
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://mail.ru");
         System.out.println("Title " + driver.getTitle());
